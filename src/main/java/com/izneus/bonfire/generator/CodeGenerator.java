4db8fb44433961fc0ca242789e25ddb7d9ac6117
sunhz <mymailcache@163.com>
2020-06-24 17:51:08 +0800

    添加依赖 添加配置文件 添加banner 添加mybatis plus代码生成器 添加ignore的目录

---
+++ src/main/java/com/izneus/bonfire/generator/CodeGenerator.java
@@ -0,0 +1,164 @@
+package com.izneus.bonfire.generator;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
+ *
+ * @author AutoGenerator
+ * @author Izneus
+ * @date 2020/06/24
+ */
+public class CodeGenerator {
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    private static String scanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入" + "表名，多个英文逗号分割，注意区分大小写" + "：");
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + "表名，多个英文逗号分割" + "！");
+    }
+
+    public static void main(String[] args) {
+        // 设置代码生成器运行用参数
+        // 数据库连接
+        String dataSourceDriverClassName = "oracle.jdbc.OracleDriver";
+        String dataSourceUrl = "jdbc:oracle:thin:@144.52.16.141:1521:orcl";
+        String dataSourceUsername = "BONFIRE";
+        String dataSourcePassword = "BONFIRE";
+        /*
+        java代码生成路径，根据需要酌情修改，
+        临时可以放在/mpg，自己手动移动生成的文件至对应目录，xml文件相同处理
+        parentPackage，包名，形如com.izneus.bonfire
+        baseDir，相对工程路径的基础目录，形如/src/main/java
+         */
+        String parentPackage = "com.izneus.bonfire";
+        String baseDir = "/mpg";
+        // xml文件生成路径，根据需要酌情修改
+        String xmlDir = "/mpg/xml/";
+
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + baseDir);
+        gc.setAuthor("CodeGenerator");
+        gc.setOpen(false);
+        gc.setServiceName("%sService");
+        gc.setEntityName("%sEntity");
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl(dataSourceUrl);
+        /// dsc.setSchemaName("public");
+        dsc.setDriverName(dataSourceDriverClassName);
+        dsc.setUsername(dataSourceUsername);
+        dsc.setPassword(dataSourcePassword);
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        /// pc.setModuleName(scanner("模块名"));
+        pc.setParent(parentPackage);
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名
+                return projectPath + xmlDir + pc.getModuleName()
+                        + "/" + tableInfo.getMapperName() + StringPool.DOT_XML;
+            }
+        });
+        /// 自定义判断是否创建文件,该配置用于判断某个类是否需要覆盖创建
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录，自定义目录用");
+                if (fileType == FileType.MAPPER) {
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    return !new File(filePath).exists();
+                }
+                // 允许生成模板文件
+                return true;
+            }
+        });
+        */
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        /// 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        /// 公共父类
+        // strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        /// 公共父类
+        // strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        // strategy.setSuperEntityColumns("id");
+        strategy.setInclude(scanner().split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
