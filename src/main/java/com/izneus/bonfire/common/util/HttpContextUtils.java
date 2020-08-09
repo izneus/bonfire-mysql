3582216863f2dadd2e7475f1f79281c2290ee9cf
sunhz <mymailcache@163.com>
2020-08-09 22:28:28 +0800

    http-context工具类

---
+++ src/main/java/com/izneus/bonfire/common/util/HttpContextUtils.java
@@ -0,0 +1,19 @@
+package com.izneus.bonfire.common.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+/**
+ * @author Izneus
+ * @date 2020/08/06
+ */
+public class HttpContextUtils {
+
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+                .getRequest();
+    }
+}
