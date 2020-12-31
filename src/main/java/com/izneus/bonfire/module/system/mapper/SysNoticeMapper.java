4562b0f988b5de1c8e1d317f80cea1da8069ac32
sunhz <mymailcache@163.com>
2020-12-31 10:56:34 +0800

    补通知功能的mapper.java

---
+++ src/main/java/com/izneus/bonfire/module/system/mapper/SysNoticeMapper.java
@@ -0,0 +1,16 @@
+package com.izneus.bonfire.module.system.mapper;
+
+import com.izneus.bonfire.module.system.entity.SysNoticeEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统通知表 Mapper 接口
+ * </p>
+ *
+ * @author Izneus
+ * @since 2020-12-10
+ */
+public interface SysNoticeMapper extends BaseMapper<SysNoticeEntity> {
+
+}
---
+++ src/main/java/com/izneus/bonfire/module/system/mapper/SysUserNoticeMapper.java
@@ -0,0 +1,16 @@
+package com.izneus.bonfire.module.system.mapper;
+
+import com.izneus.bonfire.module.system.entity.SysUserNoticeEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户通知表 Mapper 接口
+ * </p>
+ *
+ * @author Izneus
+ * @since 2020-12-10
+ */
+public interface SysUserNoticeMapper extends BaseMapper<SysUserNoticeEntity> {
+
+}
