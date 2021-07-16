fd909c11e391298f755112d142b2350a0dad3789
sunhz <mymailcache@163.com>
2021-07-16 21:43:54 +0800

    mysql移植性修改

--- src/main/java/com/izneus/bonfire/module/system/entity/SysUserEntity.java
+++ src/main/java/com/izneus/bonfire/module/system/entity/SysUserEntity.java
@@ -2,7 +2,7 @@ package com.izneus.bonfire.module.system.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
 import lombok.Data;
@@ -14,92 +14,83 @@ import lombok.EqualsAndHashCode;
  * </p>
  *
  * @author Izneus
- * @since 2020-09-08
+ * @since 2021-07-14
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@TableName("SYS_USER")
+@TableName("sys_user")
 public class SysUserEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     /**
-     * 主键
+     * id
      */
-    @TableId("ID")
     private String id;
 
     /**
      * 用户名
      */
-    @TableField("USERNAME")
     private String username;
 
     /**
      * 密码
      */
-    @TableField("PASSWORD")
     private String password;
 
     /**
      * 昵称
      */
-    @TableField("NICKNAME")
     private String nickname;
 
     /**
      * 全名
      */
-    @TableField("FULLNAME")
     private String fullname;
 
     /**
      * 电子邮件
      */
-    @TableField("EMAIL")
     private String email;
 
     /**
      * 手机号码
      */
-    @TableField("MOBILE")
     private String mobile;
 
     /**
      * 创建时间
      */
-    @TableField("CREATE_TIME")
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
-     * 修改时间
+     * 创建人
      */
-    @TableField("UPDATE_TIME")
+    @TableField(fill = FieldFill.INSERT)
+    private String createUser;
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
+
+    /**
+     * 更新人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updateUser;
 
     /**
      * 备注
      */
-    @TableField("REMARK")
     private String remark;
 
     /**
      * 账号状态
      */
-    @TableField("STATUS")
     private String status;
-
-    /**
-     * 创建人
-     */
-    @TableField("CREATE_USER")
-    private String createUser;
-
-    /**
-     * 更新人
-     */
-    @TableField("UPDATE_USER")
-    private String updateUser;
 
 
 }
