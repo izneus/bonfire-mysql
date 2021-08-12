7253314bb38b61541112bbbec37224fc7bfa8183
sunhz <mymailcache@163.com>
2021-08-12 20:08:42 +0800

    时间填充修改

--- src/main/java/com/izneus/bonfire/config/MyMetaObjectHandler.java
+++ src/main/java/com/izneus/bonfire/config/MyMetaObjectHandler.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * mybatis-plus填充器，用来自动填充数据库审计类字段，比如创建时间、更新时间等，
@@ -21,8 +21,8 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
         this.strictInsertFill(metaObject, "createUser", String.class, CurrentUserUtil.getFillUserId());
         this.strictInsertFill(metaObject, "updateUser", String.class, CurrentUserUtil.getFillUserId());
     }
@@ -30,7 +30,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
         this.strictUpdateFill(metaObject, "updateUser", String.class, CurrentUserUtil.getFillUserId());
 
     }
