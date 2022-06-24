fabd6f16eaaa79795ba4c38cba2d806aaba46db8
sunhz <mymailcache@163.com>
2022-06-24 11:25:33 +0800

    自签证书例子、https配置项 调整启动时提示的swagger地址

--- src/main/java/com/izneus/bonfire/config/MyAppEventHandler.java
+++ src/main/java/com/izneus/bonfire/config/MyAppEventHandler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.net.Inet4Address;
@@ -20,13 +21,17 @@ import java.net.UnknownHostException;
 @Slf4j
 public class MyAppEventHandler {
 
+    private final Environment env;
+
     @EventListener(ApplicationReadyEvent.class)
     public void applicationReadyEventHandler(ApplicationReadyEvent event) {
         try {
             String hostAddress = Inet4Address.getLocalHost().getHostAddress();
             String port = event.getApplicationContext().getEnvironment().getProperty("local.server.port");
-            log.info("Swagger地址：http://{}:{}/swagger-ui/index.html", hostAddress, port);
-            log.info("Swagger地址：http://localhost:{}/swagger-ui/index.html", port);
+            String isSsl = env.getProperty("server.ssl.enabled");
+            String httpStr = "true".equals(isSsl) ? "https" : "http";
+            log.info("Swagger地址：{}://{}:{}/swagger-ui/index.html", httpStr, hostAddress, port);
+            log.info("Swagger地址：{}://localhost:{}/swagger-ui/index.html", httpStr, port);
         } catch (UnknownHostException e) {
             log.error("UnknownHostException", e);
         }
